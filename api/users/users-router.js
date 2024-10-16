444ea7be53b56b1622840968f01993412891aac9
“Mariana <marysiq@hotmail.com>
2024-10-16 10:15:25 -0700

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,6 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 const router = require('express').Router()
+const { restricted } = require('../auth/auth-middleware')
 
 
 /**
@@ -25,7 +26,7 @@ const router = require('express').Router()
   }
  */
 
-  router.get('/', (req, res, next) => {
+  router.get('/', restricted, (req, res, next) => {
     res.json('users')
   })
 

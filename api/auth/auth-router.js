f32299acf8bc1d2cb2660f798fca308d9ff51192
“Mariana <marysiq@hotmail.com>
2024-10-21 13:03:03 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,17 @@
+
+
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model');
+
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require('./auth-middleware');
 
 
 /**
@@ -24,7 +36,16 @@
     "message": "Password must be longer than 3 chars"
   }
  */
-
+  router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const hashedPassword = bcrypt.hashSync(password, 12); // Hash password
+      const newUser = await Users.add({ username, password: hashedPassword }); // Add new user to the database
+      res.status(201).json(newUser); // Return new user data
+    } catch (error) {
+      next(error); // Pass errors to the error handler
+    }
+  });
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +63,18 @@
   }
  */
 
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const { username, password } = req.body;
+    Users.findBy({ username }).then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) { // Verify password
+        req.session.user = user; // Set user session
+        res.json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" }); // Wrong credentials
+      }
+    }).catch(next); // Error handling
+  });
+  
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +91,24 @@
     "message": "no session"
   }
  */
+  router.get('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(500).json({ message: 'Failed to log out' });
+        } else {
+          res.clearCookie('chocolatechip'); // Clear the session cookie
+          res.json({ message: "Logged out successfully" });
+        }
+      });
+    } else {
+      res.status(200).json({ message: "No session found" }); // No session to log out
+    }
+  });
+  
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
+
+
--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const session = require("express-session");
-const Store = require("connect-session-knex")(session);
-const knex = require('knex')(require('../knexfile').development);
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -20,27 +19,14 @@ const knex = require('knex')(require('../knexfile').development);
 
 const server = express();
 
+
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-
-server.use(session({
-  name: 'chocolatechip',
-  secret: 'shh',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24, 
-    secure: false, 
-    httpOnly: true
-  },
-  resave: false,
-  saveUninitialized: false, 
-  store: new Store({ 
-    knex, 
-    tablename: 'sessions'
-  })
-}));
-
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
